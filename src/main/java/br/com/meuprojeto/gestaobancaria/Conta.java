a0842f990db1d1676fbe186a67a47490e67894f1
Daniel <daniel.1997df@gmail.com>
2021-09-12 10:27:25 -0300

    Classes menu e conta criadas

---
+++ src/main/java/br/com/meuprojeto/gestaobancaria/Conta.java
@@ -0,0 +1,22 @@
+package br.com.meuprojeto.gestaobancaria;
+
+public class Conta {
+    int numeroConta;
+    double saldo;
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
---
+++ src/main/java/br/com/meuprojeto/gestaobancaria/Menu.java
@@ -0,0 +1,10 @@
+package br.com.meuprojeto.gestaobancaria;
+
+public class Menu {
+    public static void menu(){
+        System.out.println("1 - Criar Conta Corrente");
+        System.out.println("2 - Depósito");
+        System.out.println("3 - Saque");
+        System.out.println("4 - Consultar Saldo");
+    }
+}
